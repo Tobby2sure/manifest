026fece3edbc314887a707c1b6caed71bcb818b8
Tobby2sure <tobby2sure@gmail.com>
2026-03-27 23:45:21 +0100

    fix: downgrade privy to v2 (v3 rejects cmn* app IDs), fix embeddedWallets config

--- src/lib/privy/config.ts
+++ src/lib/privy/config.ts
@@ -1,11 +1,9 @@
 import type { PrivyClientConfig } from "@privy-io/react-auth";
 
 export const privyConfig: PrivyClientConfig = {
-  loginMethods: ["email", "google", "twitter", "wallet"],
+  loginMethods: ["email", "twitter", "wallet"],
   embeddedWallets: {
-    ethereum: {
-      createOnLogin: "all-users",
-    },
+    createOnLogin: "all-users",
   },
   appearance: {
     theme: "dark",
